7b5b233737576ef8b4a22f0b7aa1955bc96ea220
SulfurBottom <397love@gmail.com>
2023-03-20 09:35:27 +0900

    csv parser 추가

--- src/app/api/csv/route.ts
+++ src/app/api/csv/route.ts
@@ -1,25 +1,44 @@
 import fs from "fs";
+import { parse } from "csv";
+import { UnitType } from "@/app/typeDef";
+
+const parseCsv = async (
+  csvData: string
+): Promise<{ [key: string]: UnitType }> => {
+  return new Promise((resolve, reject) => {
+    parse(csvData, { columns: true }, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const jsonData: { [key: string]: UnitType } = {};
+        for (const row of data) {
+          const 멤버 = row.멤버
+            .split(",")
+            .map((member: string) => member.trim().replace(/\"/g, ""));
+          const newData: UnitType = {
+            code: row.코드,
+            unitName: row.유닛명,
+            members: 멤버,
+            memberCount: 멤버.length,
+            difficulty: row.난이도,
+            answerVariation: row.비고.split(","),
+            description: "",
+          };
+          jsonData[row.코드] = newData;
+        }
+        resolve(jsonData);
+      }
+    });
+  });
+};
 
 export async function GET(request: Request) {
-  type Row = {
-    [key: string]: string;
-  };
-
   const fileData = await fs.promises.readFile(
     "public/data/hololive_units.csv",
     "utf-8"
   );
-  const lines = fileData.trim().split("\n");
-  const headers = lines[0].split(",");
-  const rows: Row[] = [];
-  for (let i = 1; i < lines.length; i++) {
-    const values = lines[i].split(",");
-    const row: Row = {};
-    for (let j = 0; j < headers.length; j++) {
-      row[headers[j]] = values[j];
-    }
-    rows.push(row);
-  }
 
-  return new Response(JSON.stringify(rows));
+  const jsonData = await parseCsv(fileData);
+
+  return new Response(JSON.stringify(jsonData));
 }
---
+++ src/app/typeDef.ts
@@ -0,0 +1,9 @@
+export interface UnitType {
+  code: string;
+  unitName: string;
+  members: string[];
+  memberCount: number;
+  difficulty: string;
+  answerVariation: string[];
+  description: string;
+}
